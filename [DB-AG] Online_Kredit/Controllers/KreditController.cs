8b3db7535aecde1f61cf5a2748c1654018046eeb
Stankovic <stephan.stankovic@gmail.com>
2016-11-21 09:33:37 +0100

    KreditRechner 0.2

---
+++ [DB-AG] Online_Kredit/Controllers/KreditController.cs
@@ -0,0 +1,28 @@
+﻿using _DB_AG__Online_Kredit.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace _DB_AG__Online_Kredit.Controllers
+{
+    public class KreditController : Controller
+    {
+        // GET: Kredit
+        public ActionResult KreditRechner()
+        {
+            return View();
+        }
+
+
+        public ActionResult KreditRechner(KreditRechnerModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                Kunde new
+            }
+        }
+
+    }
+}
