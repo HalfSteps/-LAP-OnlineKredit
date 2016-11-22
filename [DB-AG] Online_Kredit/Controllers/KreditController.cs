5ec112e9b9be91aaec70aea84614d8868162ff65
Stankovic <stephan.stankovic@gmail.com>
2016-11-22 08:26:20 +0100

    AlphaBuild 0.5 FinanzielleSituation, Persönliche Daten, etc. eingebaut.

--- [DB-AG] Online_Kredit/Controllers/KreditController.cs
+++ [DB-AG] Online_Kredit/Controllers/KreditController.cs
@@ -31,5 +31,85 @@ namespace _DB_AG__Online_Kredit.Controllers
 
         //return View();
 
+        //}
+
+        //    public ActionResult FinanzielleSituation()
+        //    {
+        //        FinanzielleSituationModel model = new FinanzielleSituationModel()
+        //        {
+        //            ID_Knde = int.Parse(TempData["idKunde"].ToString())
+        //        };
+
+        //        return View();
+
+        //    }
+
+
+        //    public ActionResult Finanzielle Situation(FinanzielleSituationModel model)
+        //    {
+
+        //        if (ModelState.IsValid)
+        //        {
+        //            if (KreditVerwaltung.FinanzielleSituationSpeichern(model.NettoEinkommen, model.RatenVerpflichtungen, model.Wohnkosten, model.EinküfteAlimenteUnterhalt, model.Unterhaltszahlungen, model.ID_Kunde))
+        //            {
+
+        //                TempData["idKunde"] = model.ID_Kunde;
+        //                return RedirectToAction("PersönlicheDaten");
+        //            }
+        //        }
+
+        //        return View();
+
+        //    }
+
+
+        //    public ActionResult PersönlicheDaten()
+        //    {
+
+        //        PersönlicheDatenModel model = new PersönlicheDatenModel()
+        //        {
+        //            ID_Kunde = int.Parse(TempData["idKunde"].ToString())
+
+        //        };
+
+        //        return View();
+        //    }
+
+
+        //    public ActionResult PersönlicheDaten(PersönlicheDatenModel model)
+        //    {
+        //        return View();
+        //    }
+
+        //    public ActionResult Arbeitgeber()
+        //    {
+        //        return View();
+        //    }
+
+        //    public ActionResult Arbeitgeber(ArbeitgeberModel model)
+        //    {
+        //        return View();
+        //    }
+
+        //    public ActionResult KontoInformationen()
+        //    {
+        //        return View();
+        //    }
+
+        //    public ActionResult KontoInformationen(KontoInformationenModel model)
+        //    {
+        //        return View();
+        //    }
+
+        //    public ActionResult Zusammenfassung()
+        //    {
+        //        return View();
+        //    }
+
+        //    public ActionResult Zusammenfassung(ZusammenfassungModel model)
+        //    {
+        //        return View();
+        //    }
+
+        //}
     }
-}
---
+++ [DB-AG] Online_Kredit/Models/FinanzielleSituationModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace _DB_AG__Online_Kredit.Controllers
+{
+    public class FinanzielleSituationModel
+    {
+
+        public double NettoEinkommen { get; set; }
+
+        public double Wohnkosten { get; set; }
+
+        public double EinkünfteAlimenteUnterhalt { get; set; }
+
+        public double UnterhaltsZahlungen { get; set; }
+
+        public double Ratenverpflichtungen { get; set; }
+
+        public int ID_Kunde { get; set; }
+    }
+}
